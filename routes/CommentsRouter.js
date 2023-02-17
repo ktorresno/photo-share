03b8ab32ec8faa8b797f31e8d5d0dd7aa8f529cd
Karen Torres <ktorresno@gmail.com>
2023-02-17 14:19:52 -0600

    To create and show comments for a given photo.

--- routes/CommentsRouter.js
+++ routes/CommentsRouter.js
@@ -1,26 +1,39 @@
 const CommentsRouter = require("express").Router();
 const db = require("../models");
 
-CommentsRouter.route('/')
+CommentsRouter.route('/:photoID')
 .post((request, response)=>{
-    const userId = request.body.userId;
-    const photoId = request.body.photoId;
-    const content = request.body.content
-    db.comment.create({userId: userId, photoId:photoId, content:content}).then((comment)=>{
-      response.send(comment)
-    }).catch(err=>{
+    const userId = request.session.userId;
+    const photoId = request.params.photoID;
+    const content = request.body.comment;
+    db.comment.create({
+        userId: userId, 
+        photoId:photoId, 
+        content:content
+    }).then((comment)=>{
+      //response.send(comment)
+      response.redirect(`/comments${request.url}`);
+    }).catch((err )=> {
         response.send(err)
-    })
-})
+    });
+});
 
-CommentsRouter.route('/:photoId')
+CommentsRouter.route('/:photoID')
 .get((request, response)=>{
-    const photoId = request.params.photoId
-    db.comment.findAll({where:{photoId: photoId}}).then(comment=>{
-        response.send(comment)
-    }).catch(err=>{
-        response.send(err)
+    const photoId = request.params.photoID;
+    db.comment
+    .findAll({ where: { photoId: photoId } })
+    .then(comment => {
+        //response.send(comment)
+        console.log("From the router: ", JSON.stringify(comment));
+        db.photo.findAll({ where: { Id: photoId } }) 
+        .then((photo) =>{
+            response.render("comment", { photo: photo, comment: comment, requestURL: request.url});
+        });
     })
+    .catch(err => {
+        response.send(err);
+    });
 })
 
 module.exports = CommentsRouter;
