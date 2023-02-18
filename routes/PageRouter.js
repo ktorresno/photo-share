70e52861f00bed3608a7e943fed33fdb1ca84dab
Karen Torres <ktorresno@gmail.com>
2023-02-17 18:50:08 -0600

    Sanitizing files uploaded by users

--- routes/PageRouter.js
+++ routes/PageRouter.js
@@ -1,23 +1,42 @@
 const PageRouter = require('express').Router();
 const { response } = require('express');
 const db = require('../models');
+const fs = require("fs");
 //routes
-PageRouter.get('/', (req, res) => {
-    console.log("cookie for userId:", req.session.userId);
-    if (req.session.userId) {
-        db.photo.findAll()
+PageRouter.get("/", (request, response) => {
+    if (request.session.userId) {
+      const { exec } = require("child_process");
+      exec(
+        `for item in $(ls $(pwd)/public/images); do
+        if [ $( file --mime-type $(pwd)/public/images/$item -b ) != "image/jpeg" ] && [ $( file --mime-type $(pwd)/public/images/$item -b ) != "image/png" ]; then
+        echo "$(pwd)/public/images/$item"
+        fi; 
+        done;`,
+        (error, stdout, stderr) => {
+          if (stdout) {
+            fs.unlink(stdout.slice(0, -1), (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+            console.log(`Deleted ${stdout} because it wasn't an image`);
+          }
+        }
+      );
+  
+      db.photo
+        .findAll()
         .then((photos) => {
-            console.log("GET IMAGES");
-            res.render("index", {data: photos});
+          console.log("GET IMAGES");
+          response.render("index", { data: photos });
         })
-        .catch( (error) => {
-            res.send(error);
+        .catch((error) => {
+          response.send(error);
         });
     } else {
-        res.redirect('/login');
+      response.redirect("/login");
     }
-});
-
+  });
 PageRouter.get("/photo", (request, response) => {
     console.log("/photo");
     if (request.session.userId) {
@@ -27,9 +46,22 @@ PageRouter.get("/photo", (request, response) => {
     }
 });
 
+PageRouter.get("/comment/:userId", (request, response) => {
+    console.log("/comment");
+    if (request.session.userId) {
+        response.render("comment");
+    }  else {
+        response.redirect('/login');
+    }
+});
+
 PageRouter.get("/login", (request, response) => {
     console.log("LOGGIN IN!");
-    response.render("login");
+    response.render("login", {data: ""});
+});
+PageRouter.get("/badlogin", (request, response) => {
+    console.log("BAD LOGIN!");
+    response.render("login", {data: "Bad Login Credentials"});
 });
 
 PageRouter.get("/signUp", (request, response) => {
@@ -42,6 +74,10 @@ PageRouter.get("/logout", (request, response) => {
     request.session.destroy(() => {
         response.redirect("/login");
     });
+});
+
+PageRouter.get("/*", (request, response) => {
+response.render('404');
 });
 
 module.exports = PageRouter;
